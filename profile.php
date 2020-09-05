7d217f58ca191af9348eade77d847d8daed2291f
plaboncosta <plabonjoseph@gmail.com>
2020-09-05 15:28:50 +0600

    complaint modal view and terms and conditions modal done

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 include 'config.php';
 session_start();
 
-if( !$_SESSION["user_id"]){
+if (!$_SESSION["user_id"]) {
     $redirect_url = $base_url . '/index.php';
     header('Location: ' . $redirect_url);
 }
@@ -10,7 +10,7 @@ if( !$_SESSION["user_id"]){
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if($conn->connect_error){
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
@@ -22,22 +22,22 @@ $sql           = "select * from ticket_search where id = '$id';";
 $result        = $conn->query($sql);
 $ticket_search = '';
 
-if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
         $ticket_search = $row;
     }
 }
 
 $total_passenger = (!empty($ticket_search['passenger_no']) ? $ticket_search['passenger_no'] : 0) +
-                   (!empty($ticket_search['child_no']) ? $ticket_search['child_no'] : 0);
+    (!empty($ticket_search['child_no']) ? $ticket_search['child_no'] : 0);
 
 /* Fetch Passenger Data */
 $sql_two        = "select * from passengers where search_id = '$id'";
 $result_two     = $conn->query($sql_two);
 $passenger_info = array();
 
-if($result_two->num_rows > 0){
-    while($row = $result_two->fetch_assoc()){
+if ($result_two->num_rows > 0) {
+    while ($row = $result_two->fetch_assoc()) {
         array_push($passenger_info, $row);
     }
 }
@@ -47,8 +47,8 @@ $sql_three       = "select * from payments where search_id = '$id';";
 $result_three    = $conn->query($sql_three);
 $payment_details = '';
 
-if($result_three->num_rows > 0){
-    while($row = $result_three->fetch_assoc()){
+if ($result_three->num_rows > 0) {
+    while ($row = $result_three->fetch_assoc()) {
         $payment_details = $row;
     }
 }
@@ -63,8 +63,8 @@ if($result_three->num_rows > 0){
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>BANGLADESH RAILWAY TICKETS</title>
-    <link rel="shortcut icon" type="image/jpg" href="./assets/images/britslogo.png" />
-
+    <link rel="shortcut icon" type="image/jpg" href="./assets/images/britslogo.png"/>
+    
     <!--  Css Link  -->
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/icofont.min.css">
@@ -233,7 +233,7 @@ if($result_three->num_rows > 0){
                                 <div class="">
                                     <b class="ui-top"><?php echo !empty($ticket_search['date']) ?
                                             date("M j, Y",
-                                                 strtotime($ticket_search['date'])) :
+                                                strtotime($ticket_search['date'])) :
                                             '' ?></b>
                                     <p class="mb-0">Departure</p>
                                 </div>
@@ -242,14 +242,14 @@ if($result_three->num_rows > 0){
                     </div>
                     <div class="train-info shadowCustom">
                         <h4 class="ui-header mb-5"><b>Passenger Details</b></h4>
-                        <?php foreach($passenger_info as $key => $item){ ?>
+                        <?php foreach ($passenger_info as $key => $item) { ?>
                             <div class="mb-0">
                                 <div class="d-flex justify-content-between">
                                     <div class="w-50 d-flex mr-3">
-                                        <?php if( !empty($item['image_url'])){ ?>
+                                        <?php if (!empty($item['image_url'])) { ?>
                                             <img src="<?php echo $base_url . '/' . $item['image_url'] ?>"
                                                  class="profile-image" alt="">
-                                        <?php } else{ ?>
+                                        <?php } else { ?>
                                             <div class="ui-avatar"><i class="icofont-user-alt-2"></i></div>
                                         <?php } ?>
                                         <div>
@@ -268,7 +268,7 @@ if($result_three->num_rows > 0){
                                     <div class="w-50">
                                         <p><span>Date of Journey</span></p>
                                         <h5><b><?php echo !empty($ticket_search['date']) ? date("M j, Y",
-                                                                                                strtotime($ticket_search['date'])) :
+                                                    strtotime($ticket_search['date'])) :
                                                     '' ?></b></h5>
                                     </div>
                                     <div class="w-50">
@@ -310,7 +310,7 @@ if($result_three->num_rows > 0){
                                                 <p class="mb-0 ui-time">12:10 AM</p>
                                                 <p class="mb-0 ui-time"><?php echo !empty($ticket_search['date']) ?
                                                         date("d/m/Y",
-                                                             strtotime($ticket_search['date'])) :
+                                                            strtotime($ticket_search['date'])) :
                                                         '' ?></p>
                                             </div>
                                             <div>
@@ -318,7 +318,7 @@ if($result_three->num_rows > 0){
                                                 <p class="mb-0 text-right ui-time">12:10 AM</p>
                                                 <p class="mb-0 text-right ui-time"><?php echo !empty($ticket_search['date']) ?
                                                         date("d/m/Y",
-                                                             strtotime($ticket_search['date'])) :
+                                                            strtotime($ticket_search['date'])) :
                                                         '' ?></p>
                                             </div>
                                         </div>
@@ -334,7 +334,7 @@ if($result_three->num_rows > 0){
                                         <div class="d-flex justify-content-between pb-2 border-bottom-dashed mb-4">
                                             <div>
                                                 <p class="mb-2">Passenger <?php echo $total_passenger; ?></p>
-                                                <?php foreach($passenger_info as $key => $item){ ?>
+                                                <?php foreach ($passenger_info as $key => $item) { ?>
                                                     <p class="mb-2"><?php echo !empty($item['name']) ?
                                                             $item['name'] : ''; ?>
                                                         , <?php echo !empty($item['age']) ?
@@ -364,12 +364,12 @@ if($result_three->num_rows > 0){
                                             </div>
                                             <div>
                                                 <h2 class="text-right text-yellow">BDT 2980.00</h2>
-                                                <?php if( !empty($payment_details['payment_option'])){ ?>
-                                                    <?php if($payment_details['payment_option'] ==
-                                                             'bKash'){ ?>
+                                                <?php if (!empty($payment_details['payment_option'])) { ?>
+                                                    <?php if ($payment_details['payment_option'] ==
+                                                        'bKash') { ?>
                                                         <p class="text-right mb-0">MFS
                                                             - <?php echo $payment_details['payment_option']; ?></p>
-                                                    <?php } else{ ?>
+                                                    <?php } else { ?>
                                                         <p class="text-right mb-0">Credit Card - VISA</p>
                                                     <?php } ?>
                                                 <?php } ?>
@@ -532,7 +532,7 @@ if($result_three->num_rows > 0){
             </div>
             <div id="pro4">
                 <h3><b>SMS Subscription</b></h3>
-
+                
                 <div class="ui-sec-container shadowCustomMid">
                     <div class="mb-4">
                         <h3 class="mb-3"><b>Buy SMS PACK</b></h3>
@@ -594,7 +594,7 @@ if($result_three->num_rows > 0){
                             <p class="mt-2">Add a Complaint</p>
                         </div>
                         <button class="btn btn-danger px-4 mr-4 py-2 h-100 rounded-lg">
-                            <h4 class="m-0 px-4 "><b>Add</b></h4>
+                            <h4 class="m-0 px-4 " data-toggle="modal" data-target="#add-complaint-modal"><b>Add</b></h4>
                         </button>
                     </div>
                 </div>
@@ -605,7 +605,7 @@ if($result_three->num_rows > 0){
                             <p class="mt-2">View Status</p>
                         </div>
                         <button class="btn btn-success px-4 mr-4 py-2 h-100 rounded-lg">
-                            <h4 class="m-0 px-4 "><b>View</b></h4>
+                            <h4 class="m-0 px-4 " data-toggle="modal" data-target="#view-complaint-modal"><b>View</b></h4>
                         </button>
                     </div>
                 </div>
@@ -616,7 +616,7 @@ if($result_three->num_rows > 0){
                             <p class="mt-2">View Resolve</p>
                         </div>
                         <button class="btn btn-success px-4 mr-4 py-2 h-100 rounded-lg">
-                            <h4 class="m-0 px-4 "><b>View</b></h4>
+                            <h4 class="m-0 px-4 " data-toggle="modal" data-target="#resolved-complaint-modal"><b>View</b></h4>
                         </button>
                     </div>
                 </div>
@@ -662,6 +662,141 @@ if($result_three->num_rows > 0){
     </div>
 </div>
 
+<!-- Complaint Add Modal -->
+<div class="modal fade" id="add-complaint-modal" tabindex="-1" role="dialog"
+     aria-labelledby="addComplaintLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Complaint Add</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="complaint_title">Complaint Title</label>
+                            <input type="text" name="complaint_title" id="complaint_title" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="complaint_details">Complaint Details</label>
+                            <textarea name="complaint_details" class="form-control" id="complaint_details" cols="30" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <button class="btn btn-info px-4 py-2" data-dismiss="modal">Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Complaint View Modal -->
+<div class="modal fade" id="view-complaint-modal" tabindex="-1" role="dialog"
+     aria-labelledby="viewComplaintLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Complaint Status View</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header d-flex justify-content-between">
+                                <h3>Food Quality</h3>
+                                <span class="badge badge-warning" style="height: 20px;">Pending</span>
+                            </div>
+                            <div class="card-body">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam, aspernatur consequuntur dolore
+                                    doloremque doloribus ducimus excepturi hic ipsum, nesciunt nostrum nulla odio, perferendis qui quo ratione
+                                    repudiandae sapiente sint!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mt-3">
+                        <div class="card">
+                            <div class="card-header d-flex justify-content-between">
+                                <h3>Ticket Receiving Harrassment</h3>
+                                <span class="badge badge-warning" style="height: 20px;">Pending</span>
+                            </div>
+                            <div class="card-body">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam, aspernatur consequuntur dolore
+                                    doloremque doloribus ducimus excepturi hic ipsum, nesciunt nostrum nulla odio, perferendis qui quo ratione
+                                    repudiandae sapiente sint!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Resolved Complaint Modal -->
+<div class="modal fade" id="resolved-complaint-modal" tabindex="-1" role="dialog"
+     aria-labelledby="resolvedComplaintLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="resolvedComplaintLabel">Resolved Complaint</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header d-flex justify-content-between">
+                                <h3>Seat Comfortness</h3>
+                                <span class="badge badge-success" style="height: 20px;">Solved</span>
+                            </div>
+                            <div class="card-body">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam, aspernatur consequuntur dolore
+                                    doloremque doloribus ducimus excepturi hic ipsum, nesciunt nostrum nulla odio, perferendis qui quo ratione
+                                    repudiandae sapiente sint!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mt-3">
+                        <div class="card">
+                            <div class="card-header d-flex justify-content-between">
+                                <h3>Online Ticket Purchasing</h3>
+                                <span class="badge badge-success" style="height: 20px;">Solved</span>
+                            </div>
+                            <div class="card-body">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquam, aspernatur consequuntur dolore
+                                    doloremque doloribus ducimus excepturi hic ipsum, nesciunt nostrum nulla odio, perferendis qui quo ratione
+                                    repudiandae sapiente sint!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <!-- Js Link -->
 <script src="./assets/js/jquery.min.js"></script>
 <script src="./assets/js/bootstrap.min.js"></script>
@@ -670,20 +805,20 @@ if($result_three->num_rows > 0){
 <script>
     let base = "<?php echo $base_url; ?>";
     
-    function addSearchedTicketInfo(){
+    function addSearchedTicketInfo() {
         $.ajax({
             url   : base + '/db.php',
             method: 'POSt',
             data  : $("#searchTicketForm").serialize(),
-        }).done(function (response){
+        }).done(function (response) {
             let result = JSON.parse(response);
-            if (result.success){
+            if (result.success) {
                 window.location.reload();
             }
         });
     }
     
-    function getSearchedTicketData(){
+    function getSearchedTicketData() {
         $.ajax({
             url     : base + '/db.php',
             method  : 'POST',
@@ -692,8 +827,8 @@ if($result_three->num_rows > 0){
                 "action": "get_searched_ticket_data",
                 "id"    : "<?php echo $_SESSION['ticket_search_insert_id']; ?>"
             },
-        }).done(function (response){
-            if (response){
+        }).done(function (response) {
+            if (response) {
                 $("#searchTicketForm").find("select[name='departure']").val(response.departure).end();
                 $("#searchTicketForm").find("select[name='arrival']").val(response.arrival).end();
                 $("#searchTicketForm").find("input[name='date']").val(response.date).end();
@@ -705,17 +840,17 @@ if($result_three->num_rows > 0){
         });
     }
     
-    $("#e_ticket_print").click(function (){
+    $("#e_ticket_print").click(function () {
         w = window.open('mpdf.php');
         w.print();
     });
     
-    function addMealCount(id){
+    function addMealCount(id) {
         let value = $("#" + id).find('b').text();
         $("#" + id).find('b').text(parseInt(value) + 1);
     }
     
-    $(document).ready(function (){
+    $(document).ready(function () {
         getSearchedTicketData();
     });
 </script>
